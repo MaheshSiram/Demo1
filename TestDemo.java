e16644bb4424716994650aac3f0f830b0e1d9899
maheshnaidu <siramshettymahesh@email.com>
2017-03-30 21:05:23 -0700

    add

---
+++ TestDemo.java
@@ -0,0 +1,25 @@
+
+
+public class TestDemo {
+	
+	public void Demo(){
+		
+		System.out.println("This is under construction");
+		
+	}
+	
+	public void Test(){
+		System.out.println("Testing ");
+	}
+	
+
+	public static void main(String[] args) {
+		
+		TestDemo demo = new TestDemo();
+		
+		demo.Demo();
+		demo.Test();
+		
+	}
+
+}
